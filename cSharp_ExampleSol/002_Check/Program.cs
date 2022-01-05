5ddfe62725486ac3d4a3a57d2c73cbe9ccdfa03d
seongbae15 <pseongbae@gmail.com>
2022-01-05 17:22:53 +0900

    Upload check 3, 4

---
+++ cSharp_ExampleSol/002_Check/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  *  ");
+            Console.WriteLine(" *** ");
+            Console.WriteLine("*****");
+        }
+    }
+}
---
+++ cSharp_ExampleSol/004_Check/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("국어 점수를 입력하세요 ");
+            int kor = int.Parse(Console.ReadLine());
+            Console.Write("영어 점수를 입력하세요 ");
+            int eng = int.Parse(Console.ReadLine());
+            Console.Write("수학 점수를 입력하세요 ");
+            int math = int.Parse(Console.ReadLine());
+
+            int sum = kor + eng + math;
+            float avg = sum / 3f;
+
+            Console.WriteLine("국어: {0}, 영어: {1}, 수학: {2}",kor, eng, math);
+            Console.WriteLine("총점: {0}, 평균: {1}", sum, avg);
+        }
+
+    }
+}
