75e4cbde933cd7d21a8c7b53d3e6c673d2a7f672
seongbae15 <pseongbae@gmail.com>
2022-01-12 14:30:18 +0900

    Upload Check005~007

---
+++ cSharp_ExampleSol/005_Check/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a;
+            int b;
+
+            int inputNum = 0;
+            int check = 1;
+            int correct = 0;
+            while(true)
+            {
+                a = rnd.Next(1, 100);
+                b = rnd.Next(1, 100);
+
+                Console.WriteLine("{0}. 다음 두 수의 합은?", check);
+                Console.Write("{0} + {1} = ", a, b);
+                inputNum = int.Parse(Console.ReadLine());
+
+                if((a+b) == inputNum)
+                {
+                    Console.WriteLine("*** 정답 ***");
+                    correct++;
+                }
+                else
+                    Console.WriteLine("*** 오답 ***");
+
+                if(check++ >= 5)
+                {
+                    Console.WriteLine("\n*** 결과 ***");
+                    Console.WriteLine("총 정답 수:{0}", correct);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ cSharp_ExampleSol/006_Check/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndNum = rnd.Next(1, 100);
+            int inputNum = 0;
+            int check = 0;
+
+            Console.WriteLine("0~99 Up? Down? (0: Exit)");
+            while (true)
+            {
+                inputNum = int.Parse(Console.ReadLine());
+                if (0 == inputNum)
+                    break;
+                ++check;
+                if (inputNum > rndNum)
+                    Console.WriteLine("Down");
+                else if (inputNum < rndNum)
+                    Console.WriteLine("Up");
+                else
+                {
+                    Console.WriteLine("정답");
+                    Console.WriteLine("Try : {0}", check);
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
